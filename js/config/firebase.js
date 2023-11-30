f0141a377198da4de5bef707eef84c0ca9d3deb5
Anas Jahangir <md4909719@gmail.com>
2023-11-29 16:04:54 -0800

    Update firebase.js

--- js/config/firebase.js
+++ js/config/firebase.js
@@ -44,6 +44,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
+console.log(process.env.API_KEY)
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const storage = getStorage(app);
